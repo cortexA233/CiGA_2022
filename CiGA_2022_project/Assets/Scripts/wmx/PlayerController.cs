ea8b42e1576eb624df87610ac84e1d3be12ac8f5
cortexA233 <1004110666@qq.com>
2022-06-25 13:36:29 +0800

    update

---
+++ CiGA_2022_project/Assets/Scripts/wmx/PlayerController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public PlayerFSM stateMachine { private set; get; }
+    public Animator animator { private set; get; }
+    public Collider2D weaponHitbox { private set; get; }
+    public Collider2D characterHitbox { private set; get; }
+    public Rigidbody2D rigid2D { private set; get; }
+
+    public float moveSpeed;
+
+    GameObject spriteObj;
+    Vector2 curSpeed = new Vector2();
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        stateMachine = new PlayerFSM(this);
+        rigid2D = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stateMachine.currentState.HandleUpdate();
+    }
+
+    private void FixedUpdate()
+    {
+        stateMachine.currentState.HandleFixedUpdate();
+    }
+
+    public void HandleDirectionInput()
+    {
+        float dirX = Input.GetAxis("Horizontal");
+        curSpeed = rigid2D.velocity;
+        curSpeed.x = dirX * moveSpeed;
+        rigid2D.velocity = curSpeed;
+    }
+}
---
+++ CiGA_2022_project/Assets/Scripts/wmx/PlayerFSM.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public abstract class PlayerBaseState : BaseState
+{
+    protected PlayerController controller;
+}
+
+
+public class PlayerFSM : BaseFSM
+{
+    public PlayerFSM(PlayerController controller)
+    {
+        currentState = new IdleState(controller);
+    }
+    public class IdleState : PlayerBaseState
+    {
+        public IdleState(PlayerController controller)
+        {
+            this.controller = controller;
+            EnterState();
+        }
+        public override void EnterState()
+        {
+            //controller.animator.SetTrigger("isIdle");
+        }
+
+        public override void ExitState()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void HandleCollide(Collision2D collision)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void HandleFixedUpdate()
+        {
+            controller.HandleDirectionInput();
+        }
+
+        public override void HandleTrigger(Collider2D collider)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void HandleUpdate()
+        {
+
+        }
+    }
+
+    public class RunState : PlayerBaseState
+    {
+        public RunState(PlayerController controller)
+        {
+            this.controller = controller;
+            EnterState();
+        }
+        public override void EnterState()
+        {
+            //controller.animator.SetTrigger("isIdle");
+        }
+
+        public override void ExitState()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void HandleCollide(Collision2D collision)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void HandleFixedUpdate()
+        {
+            controller.HandleDirectionInput();
+        }
+
+        public override void HandleTrigger(Collider2D collider)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void HandleUpdate()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
