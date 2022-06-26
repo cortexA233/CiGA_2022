51b9db0383cca0f3d17cdd0a1785963b4ab06ad1
misuchii <shin77379@qq.com>
2022-06-26 15:09:57 +0800

    update

--- CiGA_2022_project/Assets/Scripts/dyz/FishController.cs
+++ CiGA_2022_project/Assets/Scripts/dyz/FishController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class FishController : MonoBehaviour
 {
+    private bool canMove;
+    private bool beAttracked;
     private float speed;
+    private float speedUp;
     private int lookDirection;
     private Rigidbody2D rigidbody;
-    private Vector2 begin;
 
     /// <summary>
     /// Start is called on the frame when a script is enabled just before
@@ -19,7 +21,25 @@ public class FishController : MonoBehaviour
 
         lookDirection = (Random.Range(0, 2) == 0) ? 1 : -1;
 
+        canMove = true;
+
+        beAttracked = false;
+
         speed = Random.Range(100f, 200f);
+
+        speedUp = 0.1f;
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        if (GameObject.Find("bait") == null)
+        {
+            canMove = true;
+            beAttracked = false;
+        }
     }
 
     /// <summary>
@@ -32,12 +52,28 @@ public class FishController : MonoBehaviour
 
     void Move()
     {
-        rigidbody.velocity = new Vector2(lookDirection * speed * Time.deltaTime, rigidbody.velocity.y);
+        if (canMove)
+        {
+            if (beAttracked)
+            {
+                rigidbody.velocity = new Vector2(lookDirection * speed * (1 + speedUp) * Time.deltaTime, rigidbody.velocity.y);
+            }
+            else
+            {
+                rigidbody.velocity = new Vector2(lookDirection * speed * Time.deltaTime, rigidbody.velocity.y);
+            }
+        }
+        else
+        {
+            rigidbody.velocity = Vector3.zero;
+        }
     }
 
     void Flip()
     {
         lookDirection *= -1;
+
+        transform.localScale = new Vector3(lookDirection, 0.586f, 1);
     }
 
     /// <summary>
@@ -52,6 +88,42 @@ public class FishController : MonoBehaviour
         {
             // Debug.Log("Flip");
             Flip();
+        }
+
+        if (other.CompareTag("Bait"))
+        {
+            // Debug.Log("Bait");
+            canMove = false;
+        }
+    }
+
+    /// <summary>
+    /// Sent each frame where another object is within a trigger collider
+    /// attached to this object (2D physics only).
+    /// </summary>
+    /// <param name="other">The other Collider2D involved in this collision.</param>
+    void OnTriggerStay2D(Collider2D other)
+    {
+        // Debug.Log("Stay");
+        if (other.CompareTag("AttrackRange"))
+        {
+            // Debug.Log("Attracked");
+            beAttracked = true;
+        }
+    }
+
+    /// <summary>
+    /// Sent when another object leaves a trigger collider attached to
+    /// this object (2D physics only).
+    /// </summary>
+    /// <param name="other">The other Collider2D involved in this collision.</param>
+    void OnTriggerExit2D(Collider2D other)
+    {
+        // Debug.Log("Exit");
+        if (other.CompareTag("AttrackRange") || other.CompareTag("Bait"))
+        {
+            beAttracked = false;
+            canMove = true;
         }
     }
 }
--- CiGA_2022_project/Assets/Scripts/wmx/BaitController.cs
+++ CiGA_2022_project/Assets/Scripts/wmx/BaitController.cs
@@ -7,13 +7,13 @@ public class BaitController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, 3.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
